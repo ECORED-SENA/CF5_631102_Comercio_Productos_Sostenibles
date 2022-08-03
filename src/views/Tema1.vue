96bd4f388afdfe7964c672da0b17ddb5adb1e7da
Luis Perez <ljpm9401@gmail.com>
2022-08-02 21:49:27 -0500

    Avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -11,8 +11,243 @@
       .col-md-5
         p(data-aos="fade-right" style="color:#EA3584;font-size:24px;font-weight:bold;") La expresión “mercadeo verde” se conoce también como mercadeo ambiental o mercadeo ecológico.
       .col-md-7
-        p(data-aos="fade-left") Se define como todas las actividades cuyo objetivo principal es generar y proporcionar cambios dirigidos a satisfacer las necesidades de las personas, con la menor huella negativa posible hacia el medio ambiente. Este concepto presenta mucha similitud con la definición de mercadeo tradicional, con la diferencia en causar el menor impacto ambiental negativo. 
+        p(data-aos="fade-left") Se define como todas las actividades cuyo objetivo principal es generar y proporcionar cambios dirigidos a satisfacer las necesidades de las personas, con la menor huella negativa posible hacia el medio ambiente. Este concepto presenta mucha similitud con la definición de mercadeo tradicional, con la diferencia en causar el menor impacto ambiental negativo.
 
+    p.mt-5(data-aos="fade-up") Teniendo en cuenta las afirmaciones anteriores,  Castellano et al. (2015) concluyen que el mercadeo verde o Green Marketing es el “proceso asumido por las empresas, que incorpora acciones para la obtención de un producto o servicio de forma amigable, disminuyendo o eliminando los daños que pueda ocasionar dicho proceso al medio ambiente”, considerando las etapas del ciclo de vida de un bien o servicio, es decir, desde la selección y extracción de la materia prima, hasta la reutilización o reciclaje del producto para su disposición final.
+
+    Separador
+
+    .titulo-segundo
+      h2#t_1_1 1.1 Ventajas y desventajas del mercadeo verde
+
+    p(data-aos="fade-up") Crear un negocio verde no es una labor fácil, porque, adicionalmente a las estrategias de green marketing que se deben aplicar, todo el desarrollo y operación del negocio debe estar enfocado a la protección del medio ambiente. 
+
+    .row.mt-5.cont_backgroundt1_1.pb-5(style="position:relative;")
+      .col-md-6
+        .p-5(style="position:relative;background-color:#FFE0F0;border-radius:15px;height:100%;")
+          figure.text-center.col-3.m-auto
+            img(src="@/assets/curso/tema1/2.svg" data-aos="zoom-in-up" style="position:relative;")
+          p.mt-4(data-aos="fade-up" style="position:relative;") Algunas de las ventajas del mercadeo verde son:
+          ul.lista-ul.mt-4
+            li.mb-4(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Cautiva o atrae nuevos clientes.
+            li.mb-4(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Posiciona la marca.
+            li.mb-4(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Aumenta la percepción de la calidad de productos y servicios ofrecidos a los clientes.
+            li.mb-4(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Aumenta el valor del negocio, lo que repercute directamente en un aumento en las ventas.
+            li.mb-4(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Se toma conciencia y se brinda educación ambiental a los consumidores.
+            li(data-aos="fade-up" style="position:relative")
+              i.fas.fa-check-circle(style="color:#EA3584;")
+              | Disminuye la utilización de materiales contaminantes.
+      .col-md-6
+        .p-5(style="position:relative;background-color:#EBE3F9;border-radius:15px;height:100%;")
+          figure.text-center.col-3.m-auto
+            img(src="@/assets/curso/tema1/3.svg" data-aos="zoom-in-up" style="position:relative;")
+          p.mt-4(data-aos="fade-up" style="position:relative;") Sin embargo, se encuentran también algunas desventajas en el mercadeo verde, las cuales son retos que, al momento de implementar las estrategias, se pueden superar con una buena planificación y administración:
+          ul.lista-ul.mt-4
+            li(data-aos="fade-up" style="position:relative")
+              i.fas.fa-times-circle(style="color:#6826CC;")
+              | Altos costos de inversión en la utilización de estrategias de mercadeo verde y al momento de producir los bienes y servicios que se ofrecen (costos de producción), debido a que se necesitarán realizar inversiones en el proceso productivo, usando tecnología que se encuentre a la vanguardia, para evitar posibles daños al medio ambiente.
+            li(data-aos="fade-up" style="position:relative")
+              i.fas.fa-times-circle(style="color:#6826CC;")
+              | Percepción negativa de posibles clientes e inversionistas que pueden creer que el mercadeo verde es solo optimismo o moda, lo que se traduciría en desconfianza.
+            li(data-aos="fade-up" style="position:relative")
+              i.fas.fa-times-circle(style="color:#6826CC;")
+              | Empresas renuentes al cambio.
+
+      Separador
+
+    .titulo-segundo
+      h2#t_1_2 1.2 Implementación de una estrategia de mercadeo verde en una empresa
+
+    p(data-aos="fade-up") Para el desarrollo de estrategias de mercadeo verde, se requiere conocer la información básica del mercadeo tradicional, añadiendo aspectos claves de conocimiento en materia ambiental, tales como:
+
+    .row.mt-5.cont_backgroundt1_2.pb-5(style="position:relative;")
+      .row.col-md-8.m-auto
+        .col-lg-6
+          .tarjeta-avatar-b.mb-5(data-aos="fade-up" style="position:relative")
+            .tarjeta-avatar-b__img
+              img(src='@/assets/curso/tema1/5.svg' style="position:relative")
+            .tarjeta.tarjeta--azul(style="background-color:#E8E8E8;")
+              .p-4
+                p.mb-0(style="position:relative") Comprender la problemática ambiental actual.
+        .col-lg-6
+          .tarjeta-avatar-b.mb-5(data-aos="fade-up" style="position:relative")
+            .tarjeta-avatar-b__img
+              img(src='@/assets/curso/tema1/6.svg' style="position:relative")
+            .tarjeta.tarjeta--azul(style="background-color:#ECEDF4;")
+              .p-4
+                p.mb-0(style="position:relative") La toma de conciencia frente a dicha problemática.
+
+      p.mt-5(data-aos="fade-up" style="position:relative") El mercadeo tradicional se compone de un grupo de cuatro variables llamadas las 4P:
+
+      .row.mt-5
+        .col-md-6.col-lg-3.mb-5.mb-lg-0
+          .tarjeta-avatar.d-block(data-aos="zoom-in-up")
+            img(src='@/assets/curso/tema1/7.svg' alt='')
+            .tarjeta.color-primario.d-block(style="background-color:#FFE0F0")
+              .p-4
+                h4.text-center(style="position:relative") Precio
+        .col-md-6.col-lg-3.mb-5.mb-lg-0
+          .tarjeta-avatar.d-block(data-aos="zoom-in-up")
+            img(src='@/assets/curso/tema1/8.svg' alt='')
+            .tarjeta.color-primario.d-block(style="background-color:#EBE3F9;")
+              .p-4
+                h4.text-center(style="position:relative") Producto
+        .col-md-6.col-lg-3.mb-5.mb-lg-0
+          .tarjeta-avatar.d-block(data-aos="zoom-in-up")
+            img(src='@/assets/curso/tema1/9.svg' alt='')
+            .tarjeta.color-primario.d-block(style="background-color:#ECEDF4")
+              .p-4
+                h4.text-center(style="position:relative") Plaza #[small(style="font-weight:400;") (lugar o distribución)]
+        .col-md-6.col-lg-3.mb-5.mb-lg-0
+          .tarjeta-avatar.d-block(data-aos="zoom-in-up")
+            img(src='@/assets/curso/tema1/10.svg' alt='')
+            .tarjeta.color-primario.d-block(style="background-color:#E8E8E8")
+              .p-4
+                h4.text-center(style="position:relative") Promoción #[small(style="font-weight:400;") (comunicación)]
+
+      p.mt-5(data-aos="fade-up" style="position:relative") Las cuales se adaptan según el perfil y las preferencias de las empresas.
+
+      .cajon.color-secundario.p-4.mb-4.col-md-10.m-auto.row.mt-4(style="background-color:#F6F6F6")
+        .col-md-2(style="place-self:center;")
+          figure.text-center
+            img(src="@/assets/curso/tema1/11.svg" data-aos="fade-up" style="position:relative;display:inline;max-width:90px;")
+        .col-md-10(style="place-self:center")
+          p.mb-0(data-aos="fade-up" style="position:relative") Por lo tanto, y teniendo en cuenta el contexto anterior, el mercadeo tradicional se transforma en mercadeo verde mezclando las estrategias tradicionales con las estrategias del mercadeo verde, convirtiendo las 4P en las 4P verdes.
+
+      p.mt-5(data-aos="fade-up" style="position:relative") En la siguiente tabla, se describe cada una de las 4P verdes en una estrategia de mercadeo verde:
+
+    .titulo-sexto.color-acento-contenido(style="position:relative")
+      h5 Tabla 1
+      span Mezcla del mercadeo verde
+
+    .tabla-a.color-acento-botones.mt-4 
+      table
+        caption.mt-3(style="background-color:#F6F6F6;font-weight:400") Nota. Tomada de Monteiro et al. (2015).
+        thead(style="border:0")
+          tr(style="background-color:#CFD2E1")
+            th(style="border:0;width:30%;") Elemento
+            th(style="border:0;width:;70%") Definición
+        tbody
+          tr(style="background-color:#FFFAE7")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Producto verde
+            td.p-4(style="border:0") Responde a necesidades y requerimientos ambientales. Una vez que las necesidades del cliente son consideradas necesarias para mejorar y desarrollar nuevos productos, ambientes más seguros y saludables, también son importantes y deben ser consideradas. 
+          tr(style="background-color:#fff")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Precio verde
+            td.p-4(style="border:0") Se refiere al precio especificado a la luz de las políticas de la empresa con respecto a las consideraciones ambientales impuestas por las normas e instrucciones de la empresa, o de sus iniciativas ecológicas.
+          tr(style="background-color:#FFFAE7")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Plaza (distribución)
+            td.p-4(style="border:0") Se refiere a la distribución de productos ecológicos que son adecuados para los consumidores, en términos de facilitar la entrega y garantizar procedimientos de reciclaje, que se realizan dentro de las condiciones y requerimientos ambientales. 
+          tr(style="background-color:#fff")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Promoción verde
+            td.p-4(style="border:0") Se refiere al suministro de información real acerca de los productos, de una manera que no perjudique los intereses de los consumidores.
+
+    p.mt-5(data-aos="fade-up") Profundicemos en cada una de las estrategias de mercado verde:
+
+    TabsB.color-acento-contenido.mt-4
+      .py-4.py-md-5(titulo="Producto verde")
+        h4 Producto verde
+        p Se dice que el producto cumple con parámetros ecológicos si: 
+        .row.mt-5
+          .col-lg-4
+            .p-5(style="background-color:#FFE0F0;height:100%;")
+              figure
+                img(src="@/assets/curso/tema1/12.svg" style="max-width:50px;")
+              h2.mt-3 01.
+              p Cumple la misma función de un producto convencional causando un menor impacto negativo en todo el ciclo de vida del producto.
+          .col-lg-4
+            .p-5(style="background-color:#EBE3F9;height:100%;")
+              figure
+                img(src="@/assets/curso/tema1/13.svg" style="max-width:50px;")
+              h2.mt-3 02.
+              p Se relaciona con procesos productivos de la empresa que permiten la obtención del producto final, los cuales deben regirse por prácticas ambientalmente responsables que ocasionen impactos mínimos sobre los recursos naturales. 
+          .col-lg-4
+            .p-5(style="background-color:#ECEDF4;height:100%;")
+              figure
+                img(src="@/assets/curso/tema1/14.svg" style="max-width:50px;")
+              h2.mt-3 03.
+              p Es importante considerar el envase, empaque o embalaje, el cual debe ser en lo posible reutilizable o, de otra manera, biodegradable.
+      .py-4.py-md-5(titulo="Precio verde")
+        .row
+          .col-md-3.mb-4.mb-md-0
+            figure.text-end
+              img(src='@/assets/curso/tema1/15.svg' style="max-width:200px;display:inline;")
+          .col-md-9
+            h4 Precio verde
+            p.mt-3 Con respecto al precio para los productos verdes, o responsables ambientalmente, es importante destacar que, en general, estos suelen ser más costosos que los productos convencionales. 
+            p.mt-4 Esta diferencia puede ser vista como una desventaja frente a los productos con menor precio; sin embargo, puede ser un criterio determinante sobre aquellos segmentos de mercado que valoran la exclusividad de estos productos, los cuales no se encasillan dentro de los procesos productivos tradicionales y estarían dispuestos a pagar un poco más si consideran que, personalmente, están apoyando la causa de defender el medio ambiente. Hay expectativa a futuro respecto a que se puedan disminuir los costos de producción asociados a los productos verdes; esto con el fin de que el alto precio de estos productos no desmotive a los clientes o desincentive a comprarlos.
+      .py-4.py-md-5(titulo="Plaza (distribución)")
+        .row
+          .col-md-3.mb-4.mb-md-0(style="place-self:center")
+            figure.text-center
+              img(src='@/assets/curso/tema1/16.svg' style="max-width:200px;display:inline;")
+          .col-md-9(style="place-self:center")
+            h4 Plaza (distribución)
+            p.mt-3 La distribución de un producto se relaciona con poner a disposición del consumidor los bienes que requiere, en el sitio y momento oportunos, en la cantidad apropiada, a un precio admisible para la empresa que lo produce y que sea atractivo para el consumidor. En el mercadeo verde, se requiere que la implementación de estrategia de plaza impacte de la menor forma posible el medio ambiente, generando menos residuos durante los procesos de almacenamiento, transporte y manipulación del producto.
+
+      .py-4.py-md-5(titulo="Promoción (comunicación)")
+        .row
+          .col-md-3.mb-4.mb-md-0(style="place-self:center")
+            figure.text-center
+              img(src='@/assets/curso/tema1/17.svg' style="max-width:200px;display:inline;")
+          .col-md-9(style="place-self:center")
+            h4 Promoción (comunicación)
+            p.mt-3 Un aspecto clave dentro del mercadeo verde se encuentra dentro de las tácticas de promoción de los productos verdes, pues deben estar orientadas a transmitir los beneficios ofrecidos por estos productos y, al mismo tiempo, a conocer la imagen de una empresa comprometida con los temas ambientales. Una estrategia que resulta relevante al momento de validar el compromiso ambiental de las empresas son las llamadas “etiquetas ecológicas”.
+
+    Separador
+
+    .titulo-segundo
+      h2#t_1_3 1.3 Ejemplos de mercadeo verde en el mundo
+
+    p(data-aos="fade-up") En el siguiente video, se presentan algunos ejemplos de empresas a nivel mundial relacionadas con las acciones de mezcla de mercadeo aplicadas en el marketing sostenible. ¡Adelante!
+
+    .row.mt-5.cont_backgroundt1_3.pb-5(style="position:relative;")
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/PbdVwChb71Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      Separador
+
+    .titulo-segundo
+      h2#t_1_4 1.4 Factores de éxito del mercadeo verde
+
+    p(data-aos="fade-up") Existen cinco (5) puntos claves del mercadeo verde para tener en cuenta, los cuales permiten desarrollar habilidades eficaces de marketing verde y se convierten en una guía para evitar caer en el greenwashing y cometer errores. Los cinco puntos clave se describen a continuación:
+
+    .titulo-sexto.color-acento-contenido.mt-5(style="position:relative")
+      h5 Tabla 2
+      span Los 5 puntos clave del mercadeo verde
+
+    .tabla-a.color-acento-botones.mt-4 
+      table
+        caption.mt-3(style="background-color:#F6F6F6;font-weight:400")  Nota. Tomada de Monteiro et al. (2015).
+        thead(style="border:0")
+          tr(style="background-color:#CFD2E1")
+            th(style="border:0;width:30%;") Puntos clave
+            th(style="border:0;width:;70%") Concepto
+        tbody
+          tr(style="background-color:#FFFAE7")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Intuitivo
+            td.p-4(style="border:0") Hacer mejores alternativas, accesibles y fáciles de interpretar. Esto significa hacer que los productos o servicios verdes parezcan normales. Apreciar la vida de forma sostenible parece un camino muy difícil y arduo para la mayoría de las personas. Las empresas tienen que hacer este proceso más intuitivo. 
+          tr(style="background-color:#fff")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Integrativo
+            td.p-4(style="border:0") Asociar el comercio detallista a la tecnología, la sociedad y la ecología. La idea principal de este punto radica en la sostenibilidad, a través de la combinación de desarrollo económico con desarrollo social y ambiental.
+          tr(style="background-color:#FFFAE7")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Innovador
+            td.p-4(style="border:0") Nuevos productos creativos que aporten nuevos estilos de vida. Mucha gente está diciendo que, en el futuro, la innovación ambiental y el espíritu empresarial serán como la información y la tecnología espacial a lo largo de los últimos veinte años. 
+          tr(style="background-color:#fff")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Invitador
+            td.p-4(style="border:0") La elección positiva no es una obligación. Un producto considerado verde es, en parte, un desafío de diseño. Un producto verde es a menudo mejor, más eficiente, durable, saludable, accesible, y así sucesivamente. Por lo tanto, las organizaciones deben lidiar con la cultura y los nuevos estilos de vida. 
+          tr(style="background-color:#FFFAE7")
+            td.p-4.h5.text-center(style="border:0;border-right:1px #AFAFAF solid;") Informativo
+            td.p-4(style="border:0") La falta de información afecta el comportamiento de las personas. Las bases para el desarrollo del marketing verde residen en la educación y la participación. 
 
 
 
@@ -36,4 +271,38 @@ export default {
   },
 }
 </script>
-<style lang="sass"></style>
+<style lang="sass">
+.cont_backgroundt1_1:before
+  position: absolute
+  content: ''
+  width: 115%
+  height: 100%
+  left: -7%
+  background: url(../assets/curso/tema1/1.png)
+  background-repeat: no-repeat
+  background-size: 1550px
+  background-position: bottom
+.cont_backgroundt1_2:before
+  position: absolute
+  content: ''
+  width: 115%
+  height: 100%
+  left: -7%
+  background: url(../assets/curso/tema1/4.svg)
+  background-repeat: no-repeat
+  background-size: 1550px
+.cajon.color-secundario:before
+  background-color: #12263F
+.tabs-b__tab
+  border: none
+.cont_backgroundt1_3:before
+  position: absolute
+  content: ''
+  width: 115%
+  height: 100%
+  left: -7%
+  background: url(../assets/curso/tema1/18.svg)
+  background-repeat: no-repeat
+  background-size: 1550px
+  background-position: center
+</style>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,8 +5,53 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 
+      h1 Producción sostenible
 
+    p(data-aos="fade-up") Para garantizar el avance y sostenibilidad de las sociedades modernas es necesario que las organizaciones giren su producción hacia un enfoque de consumo y producción que contribuya a reducir los efectos ambientales negativos de los sistemas de producción, distribución y consumo, pues según datos de la ONU (2015) “el progreso económico y social conseguido durante el último siglo ha estado acompañado de una degradación medioambiental que está poniendo en peligro los mismos sistemas de los que depende el desarrollo futuro (y ciertamente, la supervivencia)”. 
+
+    .row.mt-5.cont_backgroundt2_1.pb-5.p-4(style="position:relative;background-color:#EBE3F9;")
+      .col-md-4(style="place-self:center;")
+        figure.text-center
+          img(src="@/assets/curso/tema2/2.svg" data-aos="fade-right" style="display:inline;position:relative;")
+      .col-md-8(style="place-self:center;")
+        p(data-aos="fade-up" style="position:relative;") Para lo cual, se deben tener en cuenta las etapas del ciclo de vida de los productos y servicios, promoviendo al mismo tiempo, la calidad de vida de los seres humanos
+        p.mt-4(data-aos="fade-up" style="position:relative;") En esa medida, para aliviar la presión sobre los ecosistemas se deben formular y aplicar políticas de sostenibilidad económica, social y ambiental. A su vez, es necesario que las organizaciones trabajen en la gestión de una normativa de producción sostenible que propenda por la reutilización de subproductos resultantes de la cadena productiva. En ese sentido, es necesario que las organizaciones fomenten la innovación ecológica para generar una producción más limpia dentro de la concepción de ciclo de vida en la cadena de fabricación (ONU Medio Ambiente, 2017).  
+        p.mt-4.mb-0(data-aos="fade-up" style="position:relative;") La producción sostenible es la manera en que las organizaciones ponen a disposición las políticas para la preservación del medio ambiente y se acogen a las normativas y principios de sostenibilidad en el marco jurídico de la producción sostenible establecido por los países.
+
+    p.mt-5(data-aos="fade-up") En el siguiente recurso podrá conocer la relación de la producción sostenible con el uso responsable de los recursos naturales:
+
+    SlyderC.mt-5(:datos="datosSlyder1")
+
+    .titulo-segundo.mt-5
+      h2#t_2_1 2.1  Beneficios en la producción
+
+    p(data-aos="fade-up") La producción sostenible trae beneficios a mediano y largo plazo, no solo para la organización en lo que se refiere al uso razonable del capital en la compra de materias primas que tendrán mejor uso en la fabricación y mayor rentabilidad en el manejo y desempeño económico, a la par de esto, los efectos positivos de la producción sostenible se representan en el aprovechamiento eficaz de los recursos porque se disminuyen los desechos y la contaminación, incentiva el reciclaje innovando en la reutilización, lo que es conducente a generar empleos en los mercados secundarios (ONU, 2017), así como posibilita nuevas oportunidades de negocio, aunado a esto, los beneficios de la producción se traducen en los gastos operacionales de las empresas en los que tiene que ver el consumo responsable de agua y energía eléctrica, dotándolas de capacidades para mejorar la competitividad del mercado moderno.
+
+    h5.mt-5(data-aos="fade-up" style="color:#EA3584;") A continuación conocerá los beneficios de la producción sostenible:
+
+    .tarjeta.cont_slyder1.p-4.mt-4
+      SlyderA(tipo="b")
+        .row.col-md-10.m-auto
+          .col-md-7.mb-4.mb-md-0(style="place-self:center")
+            h5 Los beneficios de la producción 
+            p En torno a la sostenibilidad también se ven reflejados con el cambio administrativo–operativo de algunas organizaciones, pues con el uso de herramientas tecnológicas como las TIC, las empresas han prescindido del consumo de papel, de lugares físicos para el trabajo, ya que se puede hacer en línea.
+          .col-md-5(style="place-self:center")
+            figure
+              img(src='@/assets/curso/tema2/8.svg')
+        .row.col-md-10.m-auto
+          .col-md-7.mb-4.mb-md-0(style="place-self:center")
+            h5 Lo cual hace que los gases de efecto invernadero 
+            p En lo que tiene que ver con el traslado de personas a sus lugares de trabajo disminuya, pues los trabajos online o remotos contribuyen a la reducción de material particulado por la descongestión urbana en movilidad y uso de transporte público o privado.  
+          .col-md-5(style="place-self:center")
+            figure
+              img(src='@/assets/curso/tema2/9.png')
+        .row.col-md-10.m-auto
+          .col-md-7.mb-4.mb-md-0(style="place-self:center")
+            h5 Es decir
+            p Ya las personas no se trasladan a sus lugares de trabajo, sino que se innova en nuevas pautas de producción que permiten el consumo sostenible en cuanto a servicios como transporte, uso de combustibles fósiles y gasto de energía en equipos de cómputo encendidos las 24 horas del día. 
+          .col-md-5(style="place-self:center")
+            figure
+              img(src='@/assets/curso/tema2/10.png')
 
 
 
@@ -19,6 +64,32 @@ export default {
   data: () => ({
     indicadorTarjetaSlide: true,
     indicadorTarjetaFlip: true,
+    datosSlyder1: [
+      {
+        titulo:
+          'La producción sostenible está relacionada con el uso responsable de los recursos naturales, con el consumo equitativo de materiales tóxicos y la reducción de las emisiones de gases de efecto invernadero, que produce la transformación de los bienes tangibles y los contaminantes que producen a lo largo del ciclo de vida.',
+        texto: '',
+        imagen: require('@/assets/curso/tema2/3.jpg'),
+      },
+      {
+        titulo:
+          'Como lo sustenta la ONU (2017), el uso sostenible de los recursos debe tenerse en cuenta en todas las etapas de la cadena de valor, desde la contratación y el diseño, la fabricación, el transporte y la utilización hasta el final de la vida útil y la reutilización. ',
+        texto: '',
+        imagen: require('@/assets/curso/tema2/4.jpg'),
+      },
+      {
+        titulo:
+          'La producción sostenible se relaciona con el consumo responsable, porque a medida que se consume de forma desmesurada la producción incrementa, lo que a largo plazo trae escasez creciente por el aumento de la demanda. ',
+        texto: '',
+        imagen: require('@/assets/curso/tema2/5.jpg'),
+      },
+      {
+        titulo:
+          'Una manera de tener un consumo responsable es que el consumo de bienes manufacturados y servicios se dé en un contexto y ambiente sano y ecológicamente equilibrado, que garantice recursos para las sociedades futuras.',
+        texto: '',
+        imagen: require('@/assets/curso/tema2/6.jpg'),
+      },
+    ],
   }),
   mounted() {
     this.$nextTick(() => {
@@ -30,4 +101,29 @@ export default {
   },
 }
 </script>
-<style lang="sass"></style>
+<style lang="sass">
+.cont_backgroundt2_1:before
+  position: absolute
+  content: ''
+  width: 115%
+  height: 100%
+  left: -7%
+  background: url(../assets/curso/tema2/1.svg)
+  background-repeat: no-repeat
+  background-size: 1550px
+  background-position: center
+.slyder-c__content__header h3
+  font-weight: 500
+  line-height: 20px
+  font-size: 16px
+.slyder__btn
+  background-color: #6826CC
+  color: white
+.slyder__btn
+  background-color: #6826CC
+  color: white
+.cont_slyder1
+  background: url(../assets/curso/tema2/7.svg)
+  background-size: cover
+  background-position: center
+</style>
